55f53f761c135e8c61799e8561dc9396d8100bf4
rifqyagustino <rfqgstn@gmail.com>
2024-12-18 12:46:23 +0700

    change asset to mix in js

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -50,8 +50,8 @@
   <!-- end info section -->
 
 
-  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap.js')}}"></script>
+  <script src="{{mix('js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{mix('js/bootstrap.js')}}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
   </script>
-  <script src="{{asset('js/custom.js')}}"></script>
+  <script src="{{mix('js/custom.js')}}"></script>
